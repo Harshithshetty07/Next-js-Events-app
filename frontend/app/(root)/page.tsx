805fe648a6a70990fd6438583dd892d5adbebb0f
Harshith KV <harshith.shetty2002@gmail.com>
2024-10-19 17:02:17 +0530

    setup

---
+++ frontend/app/(root)/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="px-10 text-4xl text-red-600"> Hello world</h1>
+    </main>
+  );
+}
---
+++ frontend/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import {Poppins} from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: "--font-poppins",
+  weight: ['400', '500', '600', '700'],
+});
+
+
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "Evently is a platform for event managament.",
+  icons: {
+    icon: '../public/assets/images/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
